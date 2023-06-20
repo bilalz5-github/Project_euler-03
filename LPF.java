f1e712a4f8cc861fd146cf2645bfe1a9253af926
Bilalz <Bilalazam1678@gmail.com>
2023-06-20 14:35:41 +0300

    initalizing the file

---
+++ LPF.java
@@ -0,0 +1,32 @@
+
+public class LPF {
+
+	public static void main(String[] args) {
+	double  d = 600851475143L;
+	
+	//long b = 0 ;
+	
+	
+	for(double i=2; i<=d; i++) {
+	
+if(  d%i==0   ) {
+		
+	
+	while( d%i == 0) {
+                        d = d/i;
+	
+		System.out.println(i);
+	}
+}
+		//++i;*/
+		
+		
+		
+		
+	}
+	
+	
+//System.out.println("Party on My Mind");*/
+	}
+
+}
